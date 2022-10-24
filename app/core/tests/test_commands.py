99bfefcbdf58bfa954c4bfe8977e810ae9dad6d9
Ruhid Shukurlu <ruhids@maestro.st>
2022-10-24 23:32:26 +0400

    flake8 bugs fixed

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -17,9 +17,7 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
-
         call_command('wait_for_db')
-
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
@@ -27,8 +25,6 @@ class CommandTests(SimpleTestCase):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
-
         call_command('wait_for_db')
-
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
