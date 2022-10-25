5e97567a48da2e07ae917dd5b3e21ba390f96cf0
Ruhid Shukurlu <ruhids@maestro.st>
2022-10-25 13:59:43 +0400

    create superuser

--- app/core/models.py
+++ app/core/models.py
@@ -21,6 +21,16 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        """Create and return superuser"""
+
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 class User(AbstractBaseUser, PermissionsMixin):
     """User model in the system"""
 
