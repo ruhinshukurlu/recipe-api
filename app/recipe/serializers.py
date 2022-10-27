a018419216f4caa5ce3edc27054cb4dac4a3c806
Ruhid Shukurlu <ruhids@maestro.st>
2022-10-27 23:26:44 +0400

    recipe listing api

---
+++ app/recipe/serializers.py
@@ -0,0 +1,14 @@
+"""
+Serialziers for the resipe API
+"""
+from rest_framework import serializers
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for the Recipe"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
